03393b9c72498031bccddef656c5db494a048538
ispong <ispong@outlook.com>
2019-04-02 22:53:05 +0800

    [add] 配置项目上下文

---
+++ src/main/java/com/aicallsystem/acs/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.aicallsystem.acs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * <p>
+ *      网页访问配置
+ * </p>
+ * @author ispong
+ * @since 4/2/2019
+ * @version 1.0.0
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+
+}
